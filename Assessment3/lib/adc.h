71267cbed90ad2079f442dabd9b9a7107d4cc694
Rory Anderson <rory@jragon.co.uk>
2019-11-22 17:24:37 +0000

    add docs

--- Assessment3/lib/adc.h
+++ Assessment3/lib/adc.h
@@ -12,13 +12,37 @@
 #define ADC_INT_ENABLE NVIC_EnableIRQ(ADC_IRQn)
 #define ADC_INT_DISABLE NVIC_DisableIRQ(ADC_IRQn)
 
+/**
+ * @brief initialises ADC 1 for adc channel 1. (Pin 16 on ioboard)
+ * 
+ */
 void adc_init();
+
+/**
+ * @brief waits for adc to convert and the returns the value
+ * >> 12 instead of div by 4096 when ranging
+ * 
+ * @return uint32_t adc value (12 bit range from 0-3.3v)
+ */
 uint32_t adc_read_blocking();
 
+/**
+ * @brief configures the adc int to fire after each conversion
+ * 
+ * @param adc_int_handler adc int callback 
+ */
 void adc_int_config(void (*adc_int_handler)(uint32_t value));
 
+/**
+ * @brief starts a free running conversion (for int mode)
+ * 
+ */
 void adc_start();
 
+/**
+ * @brief stops the free running conversion
+ * 
+ */
 void adc_stop();
 
 void ADC_IRQHandler(void);
--- Assessment3/lib/func_gen.h
+++ Assessment3/lib/func_gen.h
@@ -1,3 +1,11 @@
 #include "lpc_types.h"
 
+/**
+ * @brief gens a sinewave for the DAC centered around 1.165V
+ * shifts the bits into the right pattern for putting directly in to the DACR register
+ * 
+ * @param sinewave pointer to buffer
+ * @param samples size of buffer
+ * @param amplitude_v amplitude voltage
+ */
 void func_gen_sinewave(int32_t* sinewave, int32_t samples, float amplitude_v);
